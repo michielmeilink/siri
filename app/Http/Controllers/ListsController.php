ba2c002e388a845d36c825798a43c23971923746
Michiel Meilink <michielmeilink@gmail.com>
2017-10-10 15:24:09 +0200

    Add a Controller

---
+++ app/Http/Controllers/ListsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListsController extends Controller
+{
+    public function words(){
+        return 'Woorden';
+    }
+}
